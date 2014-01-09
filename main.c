6b5ea6ab7e7adcaac9cd72fd57851cfa10f8f013
elhmn <elhmn@localhost.localdomain>
2014-01-09 10:15:45 +0100

    kie

---
+++ main.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: elhmn <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2014/01/09 10:05:57 by elhmn             #+#    #+#             */
+/*   Updated: 2014/01/09 10:05:57 by elhmn            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "stdio.h"
+#include "stdlib.h"
+#include "ft_tri.h"
+
+int		main(int argc, char **argv)
+{
+	if (argc)
+	{
+		printf("***test implementation d algo de tri, argv = %s***\n", *argv);
+	}
+	return (0);
+}
